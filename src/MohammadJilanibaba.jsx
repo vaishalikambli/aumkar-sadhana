c5634c6562e9f5697ae8bc2f5f44773a930d391e
Vaishali Kambli <Vaishali.Kambli1@outlook.com>
2021-12-22 11:08:30 -0500

    Update MohammadJilanibaba.jsx

--- src/MohammadJilanibaba.jsx
+++ src/MohammadJilanibaba.jsx
@@ -14,7 +14,221 @@ function SufiMohammadJilanibaba() {
               className="rounded img-fluid mr-5 mb-5 float-left"
             />
             <article>
-              <h4 className="mb-3">Mohammad Jilanibaba</h4>
+              <h4 className="mb-3">
+                Shri Haji Hazarat Muhammad Jilanee Baba of Baghdad
+              </h4>
+              <p>
+                2500 years ago, Shri Nathas had anticipated the catastrophe on
+                this earth in the period of Kaliyug. Shri Nathas started a
+                mission as the means to save this beautiful world created by
+                Brahmadev. Shri Gurudada re-established this mission and with
+                Shri Jagatguru Sainath Maharaj's Krupa-ashirwad, Shri
+                Shaktipeeth was established in Goa on Gudi Padwa day. This was
+                the beginning of the main purpose of world peace and human
+                welfare. In order to raise this victory banner, Shri Dada
+                received support from several Vibhutis in this mission. Among
+                the powerful Sufi Vibhutis, Param Pujya Muhammad Jilanee Baba
+                was a pioneer in this mission. His guidance and contribution
+                were invaluable. Baba`s dargah is situated in Baghdad, Iraq.
+              </p>
+              <p>
+                Jilanee Baba was born in the Gilan province of Iran on the first
+                day of Ramadan. Jilanee means "Luminous." Baba was well known
+                for his vivid devotion to matru pitru (parents), his intense
+                love for the Almighty and his creations. This radiant Vibhuti
+                was also the maternal uncle of Khwaja Moinuddin Chisti of Ajmer.
+                When Jilanee Baba was born, his mother was sixty years of age.
+                Baba spent thirty-six years of his initial life as a teacher and
+                as a peacekeeper. The later forty years were spent in giving
+                spiritual guidance to the general public. He was extremely
+                devoted and truthful.
+              </p>
+              <p>
+                Muhammad Jilanee Baba had set up his mission for social welfare
+                in Baghdad, because of which right from a common man to
+                designated spiritual saints approached Baba for guidance and
+                counselling. His teachings were focused on truthfulness,
+                authenticity and kindness to Itarejan (others) rather than
+                importance to rituals and actions; and that God-realization is
+                through perfect <strong>aachar-vichar</strong> and{" "}
+                <strong>ucchar</strong> by being honest, genuine and treating
+                Itarejan with love. Jilanee Baba had gained the title of
+                "Gousul" which was achievable only by high-level priests.
+                Jilanee Baba had spent 25 years alone in the forest in Iraq, in
+                order to achieve oneness with God. He spent innumerable
+                sleepless nights alone.
+              </p>
+              <p>
+                Jilanee Baba has often been referred to as the 'Rose' of
+                Baghdad. This is illustrated in the story below. A Rose is
+                considered to be a representation of the meditative composure by
+                many Sufi Vibhutis. In his era, there was an abundance of Sufi
+                Vibhutis in the town of Baghdad where Jilanee Baba lived. When
+                Jilanee Baba arrived at the border of Baghdad, the native
+                Vibhutis sent him an urn filled with water up to the brim;
+                implying that the city of Baghdad had an excess of Sufi Vibhutis
+                just like the brimming water in the jug and there was no space
+                for even a single more drop of water. It was wintertime and the
+                season for roses had ended. Yet, Jilanee Baba produced a fully
+                bloomed Rose using his spiritual power and placed it carefully
+                on the brimming water in the jug sending back a clear message to
+                all the Sufis of Baghdad that he can still secure his place in
+                Baghdad. They not only got his message but also realized his
+                spiritual power and greatness. They were all overjoyed and went
+                to the border of the city to welcome him and said, "Jilanee Baba
+                is our Rose and our spiritual mentor of the highest level."
+                Since then the Rose became a holy symbol of the Qadiri Sufi
+                order which is considered as an icon of the Prophet. This is why
+                all the disciples and the followers in Qadiri order stick a rose
+                into their cap.
+              </p>
+              <p>
+                Thousands of devotees came to visit Jilanee Baba. Baba would
+                distribute all the donations offered by his devotees. He would
+                give away some portion of the donations to the vendors who
+                provided him with his daily bread and other groceries. Baba
+                would always say, "Always respect the one who provides your
+                daily bread because food is none other than Parabrahma. Your
+                soul remains calm and content when your hunger is satisfied.
+                This gives you the energy to perform prayers and to do the
+                Seva." Every afternoon the shopkeeper would come to deliver four
+                Rotis to Baba. Baba would break the bread into several pieces
+                and distribute them among the crowded devotees and then he would
+                eat the remaining piece.
+              </p>
+              <p>
+                Looking at the sufferings on this earth, Param Pujya Jilanee
+                Baba became distressed even in his dargah after leaving his
+                physical body. After several years when an incomparable soul in
+                the purest form was found in Shri Guru Dada, with Almighty's
+                directions Jilanee Baba descended down to support and guide Dada
+                in his selfless mission for the well-being of others. Highly
+                committed to the Almighty, extremely disciplined and very
+                affectionate Vibhuti, ParamPujya Muhammad Jilanee Baba appeared
+                for the first time in Audumbar where Vandaniya Dada resided.
+              </p>
+              <p>
+                When Vandaniya Dada was recruited into the military, he had the
+                opportunity to visit Param Pujya Muhammad Jilanee Baba's Samadhi
+                when in Iraq. Baba guided Sri Guru Dada in this mission and
+                taught complex subject matters on Vanshvimochan and
+                Karmavimochan. When Shri Sai Adhyatmic Samiti was established,
+                in order to manage the mission flawlessly, Jilanee Baba appeared
+                in Sanchar avastha for preaching and guidance regularly for
+                almost four years. Previously when Shri Haji Malang Baba would
+                appear in trance through Dada's medium there were no fixed
+                timings for counselling (Kamkaj). Devotees would pour in at any
+                time as per their own convenience and Shri Guru Dada never
+                refused them. However, due to this in-disciplinary action,
+                Vandaniya Dada had to suffer a lot and had to miss his meals
+                many times. In order to curb such indefinite timings P.P.
+                Jilanee Baba determined a fixed scheduled time for kamkaj.
+                Jilanee Baba appeared in Sanchar avastha and finalized the
+                Kamkaj days and timings.
+              </p>
+              <p>
+                P.P.Jilanee Baba created the new rules on the requirements for
+                kamkaj - to bring along a coconut, two paan leaves, one supari,
+                and one and quarter rupee and to be present at the center half
+                an hour before the aarti. Baba explained all the rules in detail
+                to all the devotees - The purpose of being present at the center
+                half an hour before the aarti is to get rid of one’s Dushit
+                Valay (contaminated aura). Therefore the foul ring around you
+                gets dissolved and is replaced by the Daivi Valay
+                (celestial/pure aura) which exists in the center. By being
+                present for the aarti, you have already received half the kamkaj
+                due to the cosmic vibrations in the aarti. Further, the two
+                <strong>paanbida</strong> means Iha-janm (present birth) and
+                Gata-janma (previous birth), the <strong>supari</strong> means
+                your own representation and the <strong>money</strong> on the
+                Bida means your Karmas. In addition, when you register your name
+                and address for the kamkaj, at that time you are actually
+                surrendering all the defects associated with your family,
+                ancestors of your seven generations by uttering your full family
+                name; and the defects associated with your Vastu(home) by giving
+                out your full address at the feet of ShriSai Baba. Along with
+                the paan-Bida, supari Dakshina you also bring a coconut; this
+                coconut is the only fruit that bears all the five elements of
+                Panchamahabhuta Tatwa (the five elements). This is why it is
+                called Srifal. The human body also possesses these five elements
+                because of which our defects easily flow into the coconut as
+                soon as you hold it in your hand. Ultimately, all the bodily
+                defects along with Vanshdosh, Karmadosh and Vastudosh are left
+                at Baba's feet and you go home charged with Krupa Ashirwad.
+              </p>
+              <p>
+                Param Pujya Jilanee Baba also introduced Samvedana Shastra (the
+                science of healing) in this mission. He incorporated all three
+                -Sparsh Samvedana (touch), Gyan Samvedana (knowledge) and Vichar
+                Samvedana (opinions) in this mission. He appointed and trained
+                the Sewaks on how to prepare the Abhimantrit Goli, Oil and Tirth
+                by chanting the selected mantras from Rig Vedas. Eventually,
+                Vandaniya Dada simplified the ritual to provide samvedna by
+                simply chanting Shri Satguru namasmaran which had the same
+                effect as the mantras in Rig Vedas.
+              </p>
+              <p>
+                Param Pujya Jilanee Baba also started a new scheme of small
+                savings in the mission. He instructed all the devotees to
+                deposit one and quarter rupee at the center for every meal that
+                was eaten at other people’s house or ceremonies (श्राध्द
+                shraadh, marriage, naming ceremony, threading ceremony). When
+                enough money was collected it was donated to any institute that
+                needed help to run their welfare services. This way Baba showed
+                a simple method of removing the defects in the food. Later this
+                accomplishment was added in Shri Sai Swadhyay Mandal's treasure
+                by Shri Jagadguru Sainath Maharaj. The amazing thing about this
+                is that the one who receives the donation or help will have no
+                trace of the donor. This way the defects created by the wealth
+                earned are not passed on to the devotee.
+              </p>
+              <p>
+                P.P.Jilanee Baba showed how spiritual progress became possible
+                by following the disciplined diet time-table through Aahar
+                Shastra (the science of nutritious diet). Baba suggested a daily
+                meal plan taking into account everyone's likes and dislikes in
+                order to bring discipline and restrictions in the diet;
+                eventually replacing vasana in our Annamaykosh with that of
+                Ishwari element. He made sure that everyone's favourite recipe
+                was included in the week's time-table in such a way that each
+                member of the family could enjoy their favourite meal at least
+                once a week.
+              </p>
+              <p>
+                VimochanShastra (the science of liberation) was P.P. Jilanee
+                Baba's mastery. Baba suggested five-week of Puja Prasad for
+                removal of the deficiency and irregularity that developed in
+                Vanshdosh, Karmadosh, Kuladharma, Kulachaar, Kulopasana,
+                Kuladevata, Gramdevata, Vastudevta, Vastudosh, thread ceremony,
+                utilization of homeland and Black-magic effects. Every Prasad
+                and Puja vidhi that was suggested by Jilanee Baba only proved
+                his profound knowledge and deep-rooted study about our human
+                life and related sorrows. He further outlined the rules,
+                practices and responsibilities for not only the Sewaks but also
+                for the devotees who come to the center. There are a total of 27
+                instructions in the rule book and if these are memorized or
+                remembered every day then you will see definite stability in
+                your <strong>achaar-vichar-uchaar</strong>, transforming our human life into
+                Ishwarmay (celestial). P.P.Jilanee Baba had a fascinating way of
+                combining celestial principles and earthly actions when
+                delivering Mulaqats. The most important contribution in
+                ShriDada's mission made by P.P.Jilani Baba was his tremendous
+                guidance in pervasive Karmavimochan and Runamochan. Therefore
+                after the Karmavimochan and Runamochan of the devotees, their
+                <strong>JanmaKarma runanubandh</strong> responsibility was given to P.P.Jilani
+                Baba by Shri Guru Dada.
+              </p>
+              <p>
+                Param Pujya Jilanee Baba passed away at the age of 91. He had
+                spent his entire life spreading the devotion of the everlasting
+                God and doing the welfare of mankind. Such was P.P.Muhammad
+                Jilanee Baba who descended from the highest level of Saptlokas
+                down to the earth, immersed himself in the welfare of mankind,
+                driving away common man's sorrows and making tedious efforts to
+                transform human life into celestial being. In short, he can be
+                described as a blazing reflection of Duty, Faith, Love and
+                patience.
+              </p>
             </article>
           </div>
         </div>
