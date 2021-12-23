9a9c800d578f540c29152f5333abf202391299b8
Vaishali Kambli <Vaishali.Kambli1@outlook.com>
2021-12-22 23:26:57 -0500

    Update SufiAjmerSharif.jsx

--- src/SufiAjmerSharif.jsx
+++ src/SufiAjmerSharif.jsx
@@ -14,7 +14,87 @@ function SufiAjmerSharif() {
               className="rounded img-fluid mr-5 mb-5 float-left"
             />
             <article>
-              <h4 className="mb-3">Ajmer Sharif Baba</h4>
+              <h4 className="mb-3">
+                P.P. Haji-Sheik Moinuddin Chisti, all the more prominently known
+                as Khwaja Garib Nawaz, and Ajmer Sharif ( the supporter of poor
+                people).
+              </h4>
+              <p>
+                He was quite possibly the most significant of the five vibhutis
+                to contribute his ground-breaking direction to Dada in this
+                Gurumarg. Initially known as Khwaja Moinuddin Chishti, meaning
+                the saviour of trust, he was born in Sistan and raised in
+                Khorasan, Iran. He was an immediate relative of Prophet Hazrat
+                Muhammad Pegambar's parentage, which means they had a similar
+                genealogy. And P.P Muhammad Jilani was Moinuddin's maternal
+                uncle (mamaji). Directly from his youth, he got massive bliss in
+                assisting the poor and giving food. He was an image of devotion,
+                amicability and harmony. He spent his whole time on earth
+                lecturing love for every single living being and communal
+                harmony. He saw a similar fire consuming in Dada to help
+                humanity, and accordingly leaped to help him in this mission.
+              </p>
+              <p>
+                Khwaja Garib Nawaz's family was wealthy; however, he lost his
+                mom at age eight and his dad at 15. He was left with a little
+                segment of land in the appropriation of his family property. He
+                sold his property and gave all his abundance to poor people, and
+                looked for profound information. Later he got Guruagnya
+                (directions from his master) to set up his central goal and keep
+                lecturing in Ajmer, India. He took a profound interest in
+                Iterejan Runanubandh and how he can help eradicate the agonies
+                and torments that influenced his aficionado's lives. This is why
+                Dada gave the responsibility of the devotee's Iterejan
+                Runanubandha to Khwaja Garib Nawaz.
+                <br />
+                In this mission, Ajmer Sharif instructed Dada to visit Pant
+                Maharaj Balekundri. With Pant Maharaj’s instructions, Dada gave
+                distinctive tunes to all the aartis composed by Pant Maharaj in
+                "Shri Duttapremlehari, which had Shabdbrahma and Naadbrahma.
+                Khwaja Garib Nawaz consistently realized that music is the most
+                impressive medium to associate with God. With and Khwaja Garib
+                Nawaz's direction and under Pant Maharaj's Shabdabrahma, Dada
+                accomplished an enormous part in the achievement of this
+                mission.
+              </p>
+              <p>
+                It was a consolidated longing of all the Vibhutis that Dada
+                should have his own Vastu (a sacrosanct spot) for this mission.
+                They had even chosen the name "Dwarka Mai" and had communicated
+                their desire during their few Mulakhats (appearances) through
+                Dada's medium. In any case, Dada's wellbeing was disintegrating,
+                and without monetary assistance, this would not have been
+                conceivable.
+              </p>
+              <p>
+                It was a unique custom to put a cloak behind Dada at whatever
+                point Ajmer Sharif Baba came into Sanchar Avastha through Dada's
+                medium. During one of such Mulakhat by Khwaja Moinuddin Chisti,
+                he clarified the significance of raising this Vastu. He spread
+                his cloak before all the devotees and asked for financial aid, a
+                credit to help construct "Dwarka Mai." Baba much further
+                guaranteed to restore the acquired money to all the devotees.
+                With this gift and the entirety of the Vibhuti's guidance; The
+                Holy sanctuary of Dwarka Mai was raised. Even after this
+                development, there was no limit to troubles looked at by Dada
+                and the Vibhutis. They struggled in benefiting from the
+                fulfillment testament from the region. Khawja Garib Nawaz showed
+                up in Sanchar Avastha, and the devotees set the cloak behind
+                Dada. Then, the problem was cleared instantly. Is all the more
+                astonishing that through all these troublesome occasions, Dada
+                paid back all the devotees very quickly. Besides, these
+                enthusiasts who had expanded their assistance with a gift for
+                this Vastu were honoured by Khwaja Garib Nawaz himself to have
+                their own Vastu (home) today. Just Khwaja Saheb himself had this
+                incredible Ashirwad for all Vastus – this was his strength. This
+                is the reason Dada consistently considered "DwarkaMai" as the
+                property of the Vibhutis and not his own.
+              </p>
+              <p>
+                Khwaja Garib Nawaz is a symbol of compassion, service and solid
+                love. Decisively, this is the means by which Khwaja Garib Nawaz
+                assumed a key role in Gurumarg.
+              </p>
             </article>
           </div>
         </div>
