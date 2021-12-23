ebfb551379283aa7fd058e86b8064fee5a26c61c
Vaishali Kambli <Vaishali.Kambli1@outlook.com>
2021-12-22 23:26:53 -0500

    Update SufiQutbuddinBaba.jsx

--- src/SufiQutbuddinBaba.jsx
+++ src/SufiQutbuddinBaba.jsx
@@ -14,7 +14,225 @@ function SufiQutbuddinBaba() {
               className="rounded img-fluid mr-5 mb-5 float-left"
             />
             <article>
-              <h4 className="mb-3">Qutbuddin Baba</h4>
+              <h4 className="mb-3">Khwaja Qutbuddin Bakhtiyar Kaki</h4>
+              <p>
+                The arrival of this powerful Sufi Vibhuti in Shri Dada's Karya
+                was an extraordinary destiny. After the completion of
+                Karmavimochan, the Dosh (defects) of the karmas were blocked for
+                all the devotees, and their unfavourable karmas became
+                favourable. Under Qutbuddin Baba's guidance, Shri Dada attained
+                the 'Karmaadhin' avastha for all the devotees, which was
+                necessary to eventually evolve into 'Sevak' avastha. Qutbuddin
+                Baba had appeared in Sanchar avastha through Dada's medium when
+                Dada had visited his Dargah in Delhi. He said in a stern tone,
+                "I have come here to meet you all and tell you that from now
+                onwards, you will live by Dharma and not by Karma. If you still
+                wish to complain about karmas, then drown yourself in the river
+                Jamuna". Such was this powerful Sufi Vibhuti who had spent his
+                entire life doing almighty`s work with total faith and
+                sincerity.
+              </p>
+              <p>
+                Khwaja was born in a village called Osh in Iraq. During his
+                lifetime, he was popularly known as Qutbuddin Bakhtiyar Kaki.
+                His name has an essence of spirituality where Qutbuddin means
+                Dhruvtara, which means the one who is loyal to his Guru, a
+                powerful saint and a great leader who blazes a trail for others
+                to follow. Bakhtiyar means the lucky one, and this title was
+                given by his Guru Moinuddin Chisti of Ajmer (Gariban Nawaz).
+                Khwaja was so fortunate to receive his Gurus Angarkha (attire),
+                Guru's padukas (footwear) and Guru's Shirpech (headgear like a
+                cap) while his Guru was still alive. Khwaja followed every
+                instruction in the Gurukarya and lived up to the name given by
+                his Shri Sadguru. The name Kaki was given by his devotees due to
+                the following incidence:
+              </p>
+              <p>
+                KhwajaSaheb used to buy his groceries from a nearby store on a
+                credit basis. He had warned the store owner to keep track of his
+                expenses and not allow credit exceeding 30 Dirhams. Khwaja Sahib
+                used the Dakshina money that was left behind for the Fakir by
+                the devotees to pay off his loan to the store owner. He had
+                requested the owner not to allow any further credits until all
+                the money owed to him was paid off. One fine day, Khwaja Saheb
+                simply stopped taking any loans from the store owner. From that
+                day, Rotis began to appear miraculously from beneath his aasan
+                whenever they needed. This way, Khwaja was able to feed his
+                family and did not need any loan. The store owner grew concerned
+                that Khwaja must be angry with him, so he sent his wife to
+                Khawja's house to determine why Khwaja had stopped taking loans.
+                Khwaja's wife told her about the miracle Roti appearing from the
+                aasan. However, as soon as this secret out, the miracles had
+                stopped. Then Qutbuddin's Guru Khwaja Moinuddin Chisti
+                instructed his disciples to give a loan up to 500 dirhams
+                collectively to help Khwaja with his day-to-day living.
+                Eventually, when Qutbuddin's spiritual level had reached its
+                peak, he had stopped taking any kind of loans.
+              </p>
+              <p>
+                Khwaja Qutbuddin's spirituality was of such a high class that
+                all the great leaders and the saints in the Chisti order saluted
+                his status with immense respect. Qutbudin Baba was born at
+                midnight. At the time of his birth, his mother heard a divine
+                voice, "This bright light that you see is the effect of the
+                divine power that is injected into your son's soul by the
+                Almighty. Always keep this in mind and conceal this secret about
+                your son". Keeping this in mind, Khwaja's mother raised her son.
+                She read the Quran so regularly that she had it memorized
+                thoroughly. Khwaja's father had passed away when he was one and
+                a half years old. When Khwaja was just four and a half years
+                old, his mother decided to give him a primary education in the
+                Quran. Khwaja got his primary education in Quran from Qazi
+                Hamiduddin Nagauri. Later sometime Khwaja Moinuddin Chisti
+                (Garib Nawaz) arrived in their village Osh in Iraq when
+                Qutbuddin's mother took this excellent opportunity to meet the
+                Khwaja and requested him to teach her son about the Quran.
+              </p>
+              <p>
+                Such was this enormously powerful Sufi Vibhuti whose every
+                action and massive effort was clearly outlined to establish
+                Manavta Dharma (humanity) and initiate world peace. We,
+                devotees, are extremely fortunate for Qubuddin Baba's
+                contribution, guidance and support in Shri Dada's Karya. Baba
+                not only gifted us with Karan Diksha but also further ensured
+                that we attained Sevak avastha. Baba said, 'Giving Karan Diksha
+                to all the souls that are in the Parlokas is extremely
+                important. By giving Karan Diksha to one-quarter of human beings
+                on earth (Ihalok) and three-quarters of souls in the Parlokas
+                will make it possible to bring about noticeable changes in
+                Acchar-Vicchar-Ucchar on the earth'.
+              </p>
+              <p>
+                One evening a few devotees gathered and sat around Shri Dada
+                when he requested the devotees sitting in front of him to get up
+                and sit on his sides instead. This made the space in front of
+                him vacant. Shri Dada then said, "There are several souls in
+                front of us desperately waiting for Karan diskha. We have to
+                give them this Diksha today." As soon as Shri Dada said this,
+                the whole atmosphere changed suddenly from a very casual
+                conversation into a thundering and intense situation and a
+                powerful greeting-'Salam Alekum' echoed. Qutbuddin Baba had
+                appeared in Sanchar Avasta through ShriDada's medium. At this
+                time, Shri Dada had a brilliant shine on his face and the
+                devotees were unbearable to look at his astounding powerful
+                glow. But Dada held the devotee's hands and stared directly into
+                them. Qutbuddin Baba instructed the devotees of what actions
+                they required to perform. After the regular sadhana, Baba said,
+                "I have come in a possessed state of Maha Kaaran avastha.
+                Whatever was necessary to give I have given it to you devotees.
+                I will leave behind this avastha and come back." Later as
+                mentioned, he got rid of the Maha Kaaran avashta and came back
+                in Sanchar Avasta in a much milder version and the devotees were
+                able to look at Dada's face this time. It was a first time
+                experience for all the present devotees to see how incredibly
+                powerful Maha Kaaran avastha was.Then Baba said, "Today it is
+                of utmost importance to give Karan Diksha to all the souls in
+                the Parlokas. When these souls will take birth, at that time,
+                only because of Kaaran Diskha they will become aware of the
+                purpose of their life right from the moment they are born. Your
+                Guru's efforts in this mission to obtain Karan Diksha for the
+                souls in the Parlokas is highly commendable." Qutbuddin Baba's
+                discussion on Karan Diksha was highly enlightening. Baba
+                clarified how simple it was to give Karan Diksha to those souls
+                in the Parlokas who are no longer attached to any religion,
+                caste or physical body as compared to those living on earth in a
+                physical body attached to a religion.
+              </p>
+              <p>
+                All the devotees who were present received Kaaran Diksha from
+                Shri Dada. At this time several Mulakhats on the importance of
+                Karan Diksha were conducted. On one such occasion, right in the
+                midst of the seminar Vandaniya Dada suddenly started singing
+                'Jai Jai Ram Krishna Hari'. Sant Tukaram had appeared in Sanchar
+                Avastha through Shri Dada's medium and introduced himself,"I am
+                Tukarambua; your Guru has taken a very great responsibility for
+                this mission. You are all so very fortunate to be blessed by
+                such a powerful Guru's presence. I have a very humble request to
+                make to you all. Kindly request your Guru to grant Karan Diksha
+                to our Varkari devotees as well." (Varkari are those devotees
+                who go on their annual pilgrimage to Pandharpur on foot. The vow
+                is known as Vari or trip and the devotees are Varkaris). When
+                the devotees narrated the whole incident to Shri Dada, he
+                requested them to offer Bida, supari, an unshelled coconut and 1
+                and quarter rupees in the temple. The following day, Khwaja
+                Qutbuddin Baba appeared in Sanchar avastha and instructed the
+                sewaks to visit Shri Sant Gyaneshwar Samadhi in Alandi and offer
+                the above Bida after first taking the blessings from Pant
+                Maharaj in Balekundri and Shri Sainath Maharaj in Dwarkamai
+                Pune. Finally, they received the confirmation that Karan Diksha
+                will be given to all Varkaris as well. The greatest mission of
+                defining Janmakaran to all human beings was achieved by going
+                beyond one's religion. The architect and the creator of this
+                work was Khwaja Hazrat Qutbuddin Bakhtiyar Kaki.
+              </p>
+              <p>
+                Finally, one more very important contribution made by Qutbuddin
+                Baba in this mission was the funeral prayer for the departed
+                souls. He mentioned that because of Karan Diksha and with Guru
+                Krupa these souls easily attain the higher level of lokas. These
+                souls do not need any other funeral rites. <strong>Once a devotee is
+                accepted in his Guru's Jholi, the rest of the journey is fully
+                secured and taken care of by Shri Guru.</strong> In order to ensure a
+                good appropriate next birth, it is very important to conduct the
+                funeral rites. Focusing on this point Shri Qutbuddin Baba said,
+                when these last rituals are performed for the deceased person,
+                only then that soul can get a useful life in the next birth.
+                There is no exception for funeral rites. Today there is a
+                scarcity of priests who have the ability to conduct the funeral
+                rites correctly. This is why, when Qutbuddin Baba once came in
+                sancharavastha through Shri Dada's medium he composed and
+                recited the funeral prayer in his divine voice. He instructed
+                all the devotees to say this funeral prayer on the 12th day
+                after all the funeral rituals are completed. He further
+                instructed that one must place Rs 150/- at the temple altar and
+                later send the same to Shri kshetra Balekundri for annadaan
+                (food donation). Thus, with a straightforward funeral prayer and
+                an inexpensive /effortless ritual of annadaan, Qutbuddin baba
+                simplified the funeral rites for us all devotees.
+              </p>
+              <p>
+                These powerful Sufi Vibhutis from time to time explained the
+                importance of the mission and guided the devotees through Dada's
+                medium when conducting Sadhana Samelan. In one such seminar,
+                Qutbuddin Baba had given such a powerful and highly effective
+                speech that all the devotees clearly understood the importance
+                of our journey from a small puddle of karma towards the
+                magnificent ocean of Dharma. His every word pierced through
+                every devotee's soul who were present then and left a permanent
+                impression. He mentioned how selfish, self-centred many bhaktas
+                are, those who come for kaamkaaj and then easily forget their
+                Guru once they have achieved what they asked for. But a common
+                man is unaware of what actually he has benefited from his Guru
+                at this time. They have no idea or understanding of the divine
+                shelter in Vibhutis jholi that is provided by our Guru. He
+                further explained what Guru's shelter means and how Vibhutis
+                take the responsibility of taking complete care of their
+                devotee's life. A life that is born in accordance to its Karmas,
+                the materialistic attractions are so powerful that it is almost
+                impossible to be detached from its clutches. Only Guru Krupa and
+                strong devotional prayers can help a human being to break free
+                from such forces. If ego (self-pride) is uprooted and erased due
+                to Guru Krupa then the one who lives his life to the fullest by
+                surrendering totally, submitting all his joys and sorrows at his
+                Guru's feet with complete gratitude and high regards towards the
+                Almighty is definitely the one to benefit the Gurukrupaashirwad.
+                This is usually almost impossible for any common man. Manav
+                Jeevan (human life) is 25% on Ihalok (on earth) and 75% in
+                Parlok (where Earthbound spirits reside). Qutbuddin Baba gave a
+                great explanation on this through Mulakhat.
+              </p>
+              <p>
+                We human beings have the tendency to run after destructions and
+                calamities in life. It is now high time we devotees become
+                stronger and determined for the benefit of mankind. Human Beings
+                have always accused Karma for their ill fate when praying to the
+                Almighty. What one must ask for in their prayers and how one
+                must now live <strong>in accordance with Dharma instead of Karma</strong> after
+                receiving the Guru Krupa Ashirwad has been explained by Baba.
+                Qutbuddin Baba repeatedly highlighted the message on how we
+                devotees must live our life in accordance to Dharma with Guru
+                Krupa Ashirwad.
+              </p>
             </article>
           </div>
         </div>
